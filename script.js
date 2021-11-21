932c6cbdd713548d6ba36b545897a7eb59dc669a
medronas <medron@gmx.net>
2021-11-21 03:27:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+WA.room.onEnterZone('rooftop1', () => {
+    WA.room.hideLayer(layerName : roof): void;
+})
+
+WA.room.onLeaveZone('rooftop1', () => {
+    WA.room.showLayer(layerName : roof): void;
+})
