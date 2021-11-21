11cdf3d6460d962ec0e2dc2a628d7d702eb2ac26
medronas <medron@gmx.net>
2021-11-21 04:48:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-WA.room.onEnterZone('rooftop1', () => {
-    WA.room.hideLayer(layerName : roof);
+WA.room.onEnterZone('rooftop1', () => { 
+	WA.room.hideLayer(layerName : roof); 
 })
 
 WA.room.onLeaveZone('rooftop1', () => {
-    WA.room.showLayer(layerName : roof);
+  	WA.room.showLayer(layerName : roof);
 })
