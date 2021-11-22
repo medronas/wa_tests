91dfd883b05fa386e187c708f82077163a8253c1
medronas <medron@gmx.net>
2021-11-23 00:35:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-WA.room.onEnterZone('triggerzone', () => { 
-	WA.room.hideLayer('overlay'); 
+WA.room.onLeaveZone('trigger', () => {
+    WA.chat.sendChatMessage("Goodbye!", 'Mr Robot');
+	WA.room.showLayer('overlay'); 
+})
+
+WA.room.onEnterZone('trigger', () => {
+    WA.chat.sendChatMessage("Hello!", 'Mr Robot');
+	
+	WA.room.setProperty('trigger', 'Opacity', '0,5');
 })
