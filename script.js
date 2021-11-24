790569014a3498c254874a83fff7c253145ab4bb
medronas <medron@gmx.net>
2021-11-24 02:53:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ WA.room.onLeaveZone('trigger', () => {
 WA.room.onEnterZone('trigger', () => {
     WA.chat.sendChatMessage("Hello!", 'Mr Robot'),
 		WA.room.hideLayer('roof');
+});
+
+WA.room.onLeaveZone('trigger2', () => {
+    WA.chat.sendChatMessage("Goodbye!", 'Mr Robot'),
+	WA.room.showLayer('roof2'); 
+});
+
+WA.room.onEnterZone('trigger2', () => {
+    WA.chat.sendChatMessage("Hello!", 'Mr Robot'),
+		WA.room.hideLayer('roof2');
 })
